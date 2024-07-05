4de143eaefa48b2c28b4fe195fcbf024034b4c6b
SNV <sreenathsnvaltpb@gmail.com>
2024-07-05 18:25:08 +0530

    settings changed

--- loginATG/settings.py
+++ loginATG/settings.py
@@ -21,14 +21,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-=9i+z27ndy($-i)x^931o=(5whc9fk(bf&8yz=4+xmb0m-56yd'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get("DEBUG","False").lower() == "true"
 
 AUTH_USER_MODEL = 'userauth.CustomUserModel'
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS").split(" ")
 
 
 # Application definition

d7485788ace9281227ac3b4c3305a67095895348
SNV <sreenathsnvaltpb@gmail.com>
2024-07-13 18:27:06 +0530

    added mysqldb

--- loginATG/settings.py
+++ loginATG/settings.py
@@ -89,8 +89,12 @@ WSGI_APPLICATION = 'loginATG.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'hsp_atg_db',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST': 'localhost',  # Set to 'localhost' for local development
+        'PORT': '3306',  # Set to '3306' for the default MySQL port
     }
 }
 
